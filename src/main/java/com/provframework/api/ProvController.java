edc3871ac6585f87be5a63d70091ccf6d0dca467
Ryan Gross <ryan.gross.01@gmail.com>
2026-01-03 08:21:01 -0500

    Neighbor call for sparql returning valid bundle object.

--- src/main/java/com/provframework/api/ProvController.java
+++ src/main/java/com/provframework/api/ProvController.java
@@ -2,12 +2,14 @@ package com.provframework.api;
 
 import java.util.List;
 
+import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.vocabulary.PROV;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.provframework.api.sparql.SparqlDriver;
+import com.provframework.build.java.Bundle;
 
 @RestController
 @RequestMapping("/provframework")
@@ -31,5 +33,21 @@ public class ProvController {
     @GetMapping("list/agents")
     public List<String> listAgents() {
         return this.sparqlDriver.getLabels(PROV.AGENT);
+    }
+
+    @GetMapping("neighbors")
+    public Bundle getNeighbors(String label, String type) {
+        IRI typeIri;
+        if (type.toLowerCase().equals("entity")) {
+            typeIri = PROV.ENTITY;
+        } else if (type.toLowerCase().equals("activity")) {
+            typeIri = PROV.ACTIVITY;
+        } else if (type.toLowerCase().equals("agent")) {
+            typeIri = PROV.AGENT;
+        } else {
+            throw new IllegalArgumentException("Invalid type: " + type);
+        }
+        
+        return this.sparqlDriver.getNeighbors(label, typeIri);
     }
 }
